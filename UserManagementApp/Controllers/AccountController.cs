25a4f215f2e5b2b989c6ec312aa64614a3b2fedf
Anand S <anandsatheeshk@gmail.com>
2023-07-20 16:10:15 +0530

    added login and register routes to account controller

---
+++ UserManagementApp/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagementApp.Models;
+
+namespace UserManagementApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
