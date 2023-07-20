598e61e6f73c93af812071023b2f51d264573e7b
Anand S <anandsatheeshk@gmail.com>
2023-07-20 16:48:00 +0530

    index and search get routes created in dashboard controller

---
+++ UserManagementApp/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagementApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
