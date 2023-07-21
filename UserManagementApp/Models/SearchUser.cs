007df39cf4154036c8dc09f78efbcc2cd1e10d09
Anand S <anandsatheeshk@gmail.com>
2023-07-21 11:27:26 +0530

    search user model created

---
+++ UserManagementApp/Models/SearchUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementApp.Models
+{
+	public class SearchUser
+	{
+		[Required]
+		public string SearchString { get; set; } = null!;
+
+        public IEnumerable<ListUser>? Users { get; set; }
+    }
+}
