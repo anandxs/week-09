045985737c2bb69790f2f0e4bc847724eacb0066
Anand S <anandsatheeshk@gmail.com>
2023-07-21 09:06:32 +0530

    login post method created - disconnected from db register post method created - disconnected from db login view, register view updated

--- UserManagementApp/Controllers/AccountController.cs
+++ UserManagementApp/Controllers/AccountController.cs
@@ -1,30 +1,67 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using UserManagementApp.Models;
 
 namespace UserManagementApp.Controllers
 {
-    public class AccountController : Controller
-    {
-        [HttpGet]
-        public IActionResult Login()
-        {
-            return View();
-        }
+	public class AccountController : Controller
+	{
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
 
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
+		[HttpPost]
+		public IActionResult Login(LoginUser user)
+		{
+			if (ModelState.IsValid)
+			{
+				if (true/*invalid credentials logic*/)
+				{
+					ModelState.AddModelError("InvalidCredentials", "Invalid email or password");
 
-        [HttpGet]
-        public async Task<IActionResult> Logout()
-        {
-            Console.WriteLine("hi");
+					return View(user);
+				}
 
-            await HttpContext.SignOutAsync("CookieAuth");
+				//sign in logic
+				//redirect to home if user
+				//redirect to dashboard if admin
+			}
 
-            return View("Login");
-        }
-    }
+			return View(user);
+		}
+
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Register(RegisterUser user)
+		{
+			if (ModelState.IsValid)
+			{
+				if (true/*if email is already being used*/)
+				{
+					ModelState.AddModelError("EmailAlreadyExists", "Email is already being used");
+					return View(user);
+				}
+				//if not add all values to db
+
+				return RedirectToAction("Login");
+			}
+
+			return View(user);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync("CookieAuth");
+
+			return View("Login");
+		}
+	}
 }
